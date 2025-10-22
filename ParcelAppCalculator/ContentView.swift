b13dc34a9bdc6712a5693d1ed9c6406d40978b13
Mechelle <m.cobostumbaco@gmail.com>
2025-10-22 12:47:13 +0100

    Initial Commit

---
+++ ParcelAppCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParcelAppCalculator
+//
+//  Created by Mechelle De Los Angeles on 22/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParcelAppCalculator/ParcelAppCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParcelAppCalculatorApp.swift
+//  ParcelAppCalculator
+//
+//  Created by Mechelle De Los Angeles on 22/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ParcelAppCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
